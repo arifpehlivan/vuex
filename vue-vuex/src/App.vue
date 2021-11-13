4ec12c92454d42067a418862bfa2f292f85181ae
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-11-14 00:28:26 +0300

    create ProductList

---
+++ vue-vuex/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <product-list :products="products"/>
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue'
+export default {
+  name: 'app',
+  components: {
+    ProductList
+  },
+  data(){
+    return {
+      products:[
+        {title: 'Tv', price: 5000},
+        {title: 'Pc', price: 8000},
+        {title: 'Game Console', price: 15000},
+        {title: 'Phone', price: 6000},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+body{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 
+  'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #555;
+}
+</style>
---
+++ vue-vuex/src/components/ProductList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="product-list-one">
+      <h2>Product List One</h2>
+      <ul>
+          <li v-for="(product,index) in products" :key="index">
+            <span class="title">{{ product.title }}</span>
+            <span class="const"> ${{ product.price }}</span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['products'],
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+#product-list-one{
+    background: rgb(179, 29, 29);
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+
+#product-list-one ul{
+    padding: 0;
+}
+
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+    border-radius: 20px;
+}
+
+</style>
